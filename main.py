9df40c58267a1a623cdc856acfa04ba5e9ac688e
AsmoMictian <asmodeus.mictian@gmail.com>
2025-03-29 11:48:55 -0400

    Update - Added word count functionality

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ def get_book_text(file_path): #takes filepath to text file and opens it. Storing
     with open(file_path) as f:
         thewholebook = f.read()
 
-    return thewholebook
+    return thewholebook #Returns the whole book as a string.
+
+def num_words(booktext):
+    wordcount = booktext.split()
+        
+    return len(wordcount)
 
 def main():
     fp = "/home/asmodeus/bookbot/books/frankenstein.txt"
     book = get_book_text(fp)
-    print(book)
-    return
+    decount = num_words(book)
+    print(f"{decount} words found in the document")
+    return 
+
+
+
 
 main()
