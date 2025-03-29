d29ae96310fa14c5da6c0558642a3faee0dd4dd5
AsmoMictian <asmodeus.mictian@gmail.com>
2025-03-29 12:47:08 -0400

    Completed lesson 'Refactor'. Created stats.py to house computation code.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import num_words
+
 def get_book_text(file_path): #takes filepath to text file and opens it. Storing the file in a var.
     thewholebook = ""
     with open(file_path) as f:
@@ -5,19 +7,12 @@ def get_book_text(file_path): #takes filepath to text file and opens it. Storing
 
     return thewholebook #Returns the whole book as a string.
 
-def num_words(booktext):
-    wordcount = booktext.split()
-        
-    return len(wordcount)
-
 def main():
     fp = "/home/asmodeus/bookbot/books/frankenstein.txt"
     book = get_book_text(fp)
     decount = num_words(book)
     print(f"{decount} words found in the document")
     return 
-
-
 
 
 main()
---
+++ stats.py
@@ -0,0 +1,4 @@
+def num_words(booktext):
+    wordcount = booktext.split()
+        
+    return len(wordcount)
